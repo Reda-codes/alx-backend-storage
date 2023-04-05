c6e7233ba7aa2d9b2da549d08ee301e683a764cd
Reda <muhammadredabh@gmail.com>
2023-04-05 15:28:39 +0000

    3 fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,4 +2,4 @@
 SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', style)
-ORDER BY lifespan DESC, band_name DESC;
+ORDER BY lifespan DESC;
