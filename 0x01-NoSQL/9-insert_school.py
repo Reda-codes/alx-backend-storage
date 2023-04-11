e290a73bb7f749ffae3dd6b66d0553c0d39fbd38
Reda <muhammadredabh@gmail.com>
2023-04-11 14:30:56 +0000

    9. fix id

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,4 +5,5 @@
 def insert_school(mongo_collection, **kwargs):
     ''' insert_school function'''
     newDict = {key: value for key, value in kwargs.items()}
-    mongo_collection.insert_one(newDict).inserted_id
+    _id = mongo_collection.insert_one(newDict)
+    return _id.inserted_id
