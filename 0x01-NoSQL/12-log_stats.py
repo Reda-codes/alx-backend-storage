548de0da3c01b077ad2f77de7a6cf7e256a1ff1c
Reda <muhammadredabh@gmail.com>
2023-04-11 15:39:52 +0000

    fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
     print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
     print("\tmethod GET: {}".format(collection.count_documents({"method": "GET"})))
     print("\tmethod POST: {}".format(collection.count_documents({"method": "POST"})))
     print("\tmethod PUT: {}".format(collection.count_documents({"method": "PUT"})))
