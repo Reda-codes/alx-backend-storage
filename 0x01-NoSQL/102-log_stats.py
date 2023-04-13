e0d2d7a0a7233433b0729408398f04576db2bc60
Reda <muhammadredabh@gmail.com>
2023-04-13 13:44:27 +0000

    adv test

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -14,14 +14,18 @@ if __name__ == "__main__":
     print("\tmethod PATCH: {}".format(collection.count_documents({"method": "PATCH"})))
     print("\tmethod DELETE: {}".format(collection.count_documents({"method": "DELETE"})))
     print("{} status check".format(collection.count_documents({"path": "/status"})))
+    pipeline = [
+        { "$group": {
+            "_id": "$ip",
+            "count": { "$sum": 1 }
+        }},
+        { "$sort": { "count": -1 }}
+    ]
+    results = collection.aggregate(pipeline)
     print("IPs:")
-    print("\t172.31.63.67: {}".format(collection.count_documents({"ip": "172.31.63.67"})))
-    print("\t172.31.2.14: {}".format(collection.count_documents({"ip": "172.31.2.14"})))
-    print("\t172.31.29.194: {}".format(collection.count_documents({"ip": "172.31.29.194"})))
-    print("\t69.162.124.230: {}".format(collection.count_documents({"ip": "69.162.124.230"})))
-    print("\t64.124.26.109: {}".format(collection.count_documents({"ip": "64.124.26.109"})))
-    print("\t64.62.224.29: {}".format(collection.count_documents({"ip": "64.62.224.29"})))
-    print("\t34.207.121.61: {}".format(collection.count_documents({"ip": "34.207.121.61"})))
-    print("\t47.88.100.4: {}".format(collection.count_documents({"ip": "47.88.100.4"})))
-    print("\t45.249.84.250: {}".format(collection.count_documents({"ip": "45.249.84.250"})))
-    print("\t216.244.66.228: {}".format(collection.count_documents({"ip": "216.244.66.228"})))
+    counter = 0
+    for result in results:
+        print("\t{}: {}".format(result['_id'], result['count']))
+        counter += 1
+        if counter == 10:
+            break
